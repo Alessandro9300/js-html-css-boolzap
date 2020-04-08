bdd4379c2de5ef6cb00e11859ddec26dff0e51a0
Alessandro9300 <aandreoli93@gmail.com>
2020-04-08 12:04:44 +0200

    fix send message

--- script/script.js
+++ script/script.js
@@ -3,26 +3,37 @@
 $(document).ready(function(){
 
 
-var input = $("#text-send");
+  var input = $("#text-send");
 
 
-input.keypress(function(event){
+  function sendMessage(messaggio){
 
+    var messaggio = input.val();
+  if (messaggio != ""){
 
-  var messaggio = input.val();
-
-
-  if (event.keyCode == 13){
-
-    $(".display-chat").append("<div class='messaggio inviato'>" + messaggio + "</div>");
-    input.val("");
-    console.log("all'invio il messaggio valeva" + messaggio);
+      $(".display-chat").append("<div class='messaggio inviato'>" + messaggio + "</div>");
+      input.val("");
+      console.log("all'invio il messaggio valeva" + messaggio);
+      $(".fas.fa-microphone").css("display", "block");
+      $(".fas.fa-paper-plane").css("display", "none");
+  }
   }
 
 
+  input.keypress(function(event){
 
+    $(".fas.fa-microphone").css("display", "none");
+    $(".fas.fa-paper-plane").css("display", "block");
 
-})
+    if (event.keyCode == 13){
+      sendMessage();
+    }
+
+  })
+
+  $(".fas.fa-paper-plane").click(
+    sendMessage
+  );
 
 
 
